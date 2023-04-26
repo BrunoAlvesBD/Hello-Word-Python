ee7acd6b16a50a99ac26b598d8b5810e0fe63f96
BrunoAlvesBD <116902542+BrunoAlvesBD@users.noreply.github.com>
2023-04-26 20:28:36 +0000

    It creates an add numbers operation

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 
-print("Hello Word");
+#print("Hello Word");
+
+#-----------------------------------------------------
+#line = input();
+#total_words = line.count(' ') + 1;
+#print(total_words);
+
+#PI = 3.141592653589793
+
+#radius = int(input());
+
+#heigth =int(input());
+
+#volume = (PI * radius ** 2 * heigth) / 3;
+
+#print(volume);
+#-----------------26/04---------------------
+from operators.operators import add
+print('hello word')
+#print(add(1, 2))
+
+print(add(1, 3.2))
